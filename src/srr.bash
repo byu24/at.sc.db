de3b43cc5c7e4ccf202a9f61fbcd0e22e0495ec5
Brenda Yu <36432262+byu24@users.noreply.github.com>
2019-06-04 19:41:29 -0700

    update 6/4

---
+++ src/srr.bash
@@ -0,0 +1,7 @@
+#!/bin/bash
+export PATH=$PATH:/global/projectb/scratch/byu24/bin/sratoolkit.2.9.6-1-ubuntu64/bin
+
+fasterq-dump SRR8086586
+fasterq-dump SRR8257106
+fasterq-dump SRR8206664
+fasterq-dump SRR8485805
