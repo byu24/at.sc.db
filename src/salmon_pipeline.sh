1d150e4f3b33c0d933f192cbee4f296890923418
Brenda Yu <36432262+byu24@users.noreply.github.com>
2019-06-30 19:38:00 -0700

    6/30

--- src/salmon_pipeline.sh
+++ src/salmon_pipeline.sh
@@ -14,9 +14,9 @@ export PATH=$PATH:$BSCRATCH/bin/salmon/bin
 
 if [[ $technology -eq "DropSeq" ]]; then
   ${salmon_path}/salmon alevin -l ISR -1 ${file_prefix}_rRNA_adapter_filtered_1.fastq -2 ${file_prefix}_R2.fastq --dropseq -i ${index_path}/at10_index -p 10 -o ${data_path}/${prefix} --tgMap ${index_path}/at10_tgMap.tsv --dumpMtx ${data_path}/${prefix}
-elif [[ $technology -eq "10xV2" ]]; then
+elif [[ $technology -eq "TenxV2" ]]; then
   ${salmon_path}/salmon alevin -l ISR -1 ${file_prefix}_rRNA_adapter_filtered_1.fastq -2 ${file_prefix}_R2.fastq --chromium -i ${index_path}/at10_index -p 10 -o ${data_path}/${prefix} --tgMap ${index_path}/at10_tgMap.tsv --dumpMtx ${data_path}/${prefix}
-elif [[$technology -eq "10xV3" ]]; then
+elif [[$technology -eq "TenxV3" ]]; then
   ${salmon_path}/salmon alevin -l ISR -1 ${file_prefix}_rRNA_adapter_filtered_1.fastq -2 ${file_prefix}_R2.fastq --chromium -i ${index_path}/at10_index -p 10 -o ${data_path}/${prefix} --tgMap ${index_path}/at10_tgMap.tsv --dumpMtx ${data_path}/${prefix}
 fi
 
