97302f73600c46978ff1063787011ac2c3faa8a5
Brenda Yu <36432262+byu24@users.noreply.github.com>
2019-06-30 17:42:46 -0700

    6/30

--- src/salmon_pipeline.sh
+++ src/salmon_pipeline.sh
@@ -11,13 +11,13 @@ file_prefix=$BSCRATCH/at.sc.db/scratch/${prefix}
 data_path=$BSCRATCH/at.sc.db/data
 
 if [[ $technology -eq "DropSeq" ]]; then
-  protocol=dropseq
+  ${protocol}=dropseq
 elif [[ $technology -eq "10x_V2" ]]; then
-  protocol=chromium
+  ${protocol}=chromium
 elif [[$technology -eq "10x_V3" ]]; then
-  protocol=chromium
+  ${protocol}=chromium
 fi
 
 export PATH=$PATH:$BSCRATCH/bin/salmon/bin
 
-${salmon_path}/salmon alevin -l ISR -1 ${file_prefix}_rRNA_adapter_filtered_1.fastq -2 ${file_prefix}_R2.fastq --protocol -i ${index_path}/at10_index -p 10 -o ${data_path}/${file_prefix} --tgMap ${index_path}/at10_tgMap.tsv --dumpMtx ${data_path}/${file_prefix}
+${salmon_path}/salmon alevin -l ISR -1 ${file_prefix}_rRNA_adapter_filtered_1.fastq -2 ${file_prefix}_R2.fastq --${protocol} -i ${index_path}/at10_index -p 10 -o ${data_path}/${file_prefix} --tgMap ${index_path}/at10_tgMap.tsv --dumpMtx ${data_path}/${file_prefix}
