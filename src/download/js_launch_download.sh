ec1b6fde1d72b66d25c5dab6050d0b97954658f3
Brenda Yu <36432262+byu24@users.noreply.github.com>
2019-07-02 19:48:29 -0700

    Update js_launch_download.sh

--- src/download/js_launch_download.sh
+++ src/download/js_launch_download.sh
@@ -1,4 +1,4 @@
-sbatch $BSCRATCH/src/download/download_jsh_016.bs &
-sbatch $BSCRATCH/src/download/download_js_017.bs &
-sbatch $BSCRATCH/src/download/download_js_018.bs &
+sbatch $BSCRATCH/at.sc.db/src/download/download_jsh_016.bs &
+sbatch $BSCRATCH/at.sc.db/src/download/download_js_017.bs &
+sbatch $BSCRATCH/at.sc.db/src/download/download_js_018.bs &
 
