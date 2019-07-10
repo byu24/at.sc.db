c303f12a6951f47b90ded5cb595bc06a2fa0bfd7
Brenda Yu <36432262+byu24@users.noreply.github.com>
2019-07-10 16:16:03 -0700

    Delete js_launch_download.sh

--- src/download/js_launch_download.sh
+++
@@ -1,4 +0,0 @@
-sbatch $BSCRATCH/at.sc.db/src/download/download_jsh_016.bs &
-sbatch $BSCRATCH/at.sc.db/src/download/download_js_017.bs &
-sbatch $BSCRATCH/at.sc.db/src/download/download_js_018.bs &
-
