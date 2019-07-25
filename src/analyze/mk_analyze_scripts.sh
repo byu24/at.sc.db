a287a56293403d31632bdaaead48026575b0570f
Brenda Yu <36432262+byu24@users.noreply.github.com>
2019-07-24 23:02:16 -0700

    Update mk_analyze_scripts.sh

--- src/analyze/mk_analyze_scripts.sh
+++ src/analyze/mk_analyze_scripts.sh
@@ -17,40 +17,40 @@ while [ "$i" -lt "$len" ]; do
   echo "" >> src/analyze/${lib_name}_analyze.r
   
   echo "setwd('"/global/projectb/scratch/byu24/at.sc.db/scratch"')" >> src/analyze/${lib_name}_analyze.r
-  echo "library(""Seurat"")" >> src/analyze/${lib_name}_analyze.r
+  echo "library('"Seurat"')" >> src/analyze/${lib_name}_analyze.r
   echo "library(ggplot2)" >> src/analyze/${lib_name}_analyze.r
   echo "" >> src/analyze/${lib_name}_analyze.r
 
-  echo "${lib_name}.data<-Read10X(data.dir = ""/global/projectb/scratch/byu24/at.sc.db/scratch/${lib_name}/${lib_name}_star.Solo.out"")" >> src/analyze/${lib_name}_analyze.r
-  echo "${lib_name}<- CreateSeuratObject(counts = ${lib_name}.data, project = ""${lib_name}"")" >> src/analyze/${lib_name}_analyze.r
-  echo "${lib_name} <- NormalizeData(${lib_name}, normalization.method = ""LogNormalize"", scale.factor = 10000)" >> src/analyze/${lib_name}_analyze.r
+  echo "${lib_name}.data<-Read10X(data.dir = '"/global/projectb/scratch/byu24/at.sc.db/scratch/${lib_name}/${lib_name}_star.Solo.out"')" >> src/analyze/${lib_name}_analyze.r
+  echo "${lib_name}<- CreateSeuratObject(counts = ${lib_name}.data, project = '"${lib_name}"')" >> src/analyze/${lib_name}_analyze.r
+  echo "${lib_name} <- NormalizeData(${lib_name}, normalization.method = '"LogNormalize"', scale.factor = 10000)" >> src/analyze/${lib_name}_analyze.r
   echo "" >> src/analyze/${lib_name}_analyze.r
 
-  echo "${lib_name} <- FindVariableFeatures(${lib_name}, selection.method = ""vst"", nfeatures = 2000)" >> src/analyze/${lib_name}_analyze.r
+  echo "${lib_name} <- FindVariableFeatures(${lib_name}, selection.method = '"vst"', nfeatures = 2000)" >> src/analyze/${lib_name}_analyze.r
   echo "${lib_name} <- ScaleData(${lib_name})" >> src/analyze/${lib_name}_analyze.r
   echo "" >> src/analyze/${lib_name}_analyze.r
 
   echo "${lib_name} <- RunPCA(${lib_name}, features = VariableFeatures(object = ${lib_name}))" >> src/analyze/${lib_name}_analyze.r
-  echo "print(lib_name[[""pca""]], dims = 1:5, nfeatures = 5)" >> src/analyze/${lib_name}_analyze.r
+  echo "print(lib_name[['"pca"']], dims = 1:5, nfeatures = 5)" >> src/analyze/${lib_name}_analyze.r
   echo "" >> src/analyze/${lib_name}_analyze.r
 
-  echo "png(file=""/global/projectb/scratch/byu24/at.sc.db/scratch/analysis/${lib_name}_vizdim.png"")" >> src/analyze/${lib_name}_analyze.r
-  echo "VizDimLoadings(${lib_name}, dims = 1:2, reduction = ""pca"")" >> src/analyze/${lib_name}_analyze.r
+  echo "png(file='"/global/projectb/scratch/byu24/at.sc.db/scratch/analysis/${lib_name}_vizdim.png"')" >> src/analyze/${lib_name}_analyze.r
+  echo "VizDimLoadings(${lib_name}, dims = 1:2, reduction = '"pca"')" >> src/analyze/${lib_name}_analyze.r
   echo "dev.off()" >> src/analyze/${lib_name}_analyze.r
   echo "" >> src/analyze/${lib_name}_analyze.r
 
-  echo "png(file=""/global/projectb/scratch/byu24/at.sc.db/scratch/analysis/${lib_name}_elbow.png"")" >> src/analyze/${lib_name}_analyze.r
+  echo "png(file='"/global/projectb/scratch/byu24/at.sc.db/scratch/analysis/${lib_name}_elbow.png"')" >> src/analyze/${lib_name}_analyze.r
   echo "ElbowPlot(${lib_name})" >> src/analyze/${lib_name}_analyze.r
   echo "dev.off()" >> src/analyze/${lib_name}_analyze.r
   echo "" >> src/analyze/${lib_name}_analyze.r
 
   echo "${lib_name} <- RunUMAP(${lib_name}, dims = 1:10)" >> src/analyze/${lib_name}_analyze.r
-  echo "png(file=""/global/projectb/scratch/byu24/at.sc.db/scratch/analysis/${lib_name}_dimplot.png"")" >> src/analyze/${lib_name}_analyze.r
-  echo "DimPlot({lib_name}, reduction = ""umap"")" >> src/analyze/${lib_name}_analyze.r
+  echo "png(file='"/global/projectb/scratch/byu24/at.sc.db/scratch/analysis/${lib_name}_dimplot.png"')" >> src/analyze/${lib_name}_analyze.r
+  echo "DimPlot({lib_name}, reduction = '"umap"')" >> src/analyze/${lib_name}_analyze.r
   echo "dev.off()" >> src/analyze/${lib_name}_analyze.r
   echo "" >> src/analyze/${lib_name}_analyze.r
 
-  echo "saveRDS(${lib_name}, file = ""/global/projectb/scratch/byu24/at.sc.db/scratch/analysis/${lib_name}.rds"")" >> src/analyze/${lib_name}_analyze.r
+  echo "saveRDS(${lib_name}, file = '"/global/projectb/scratch/byu24/at.sc.db/scratch/analysis/${lib_name}.rds"')" >> src/analyze/${lib_name}_analyze.r
 
   echo "nohup R CMD BATCH $BSCRATCH/at.sc.db/src/analyze/${lib_name}_analyze.r $BSCRATCH/at.sc.db/log/analyze_${lib_name}.Rout &" >> src/analyze/launch_analyze_scripts.sh
 
