f749fca414fc78211b30ba8e9682f247b6e28cde
Brenda Yu <36432262+byu24@users.noreply.github.com>
2019-08-01 16:01:46 -0700

    Update plot_sobj.R

--- src/analyze/plot_sobj.R
+++ src/analyze/plot_sobj.R
@@ -8,3 +8,4 @@ options(future.globals.maxSize = 100000 * 1024^2)
 #Load all R objects
 at.integrated = readRDS(file = "/global/projectb/scratch/byu24/at.sc.db/scratch/robjects/at_integrated.rds")
 
+at.integrated
